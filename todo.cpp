66517d4123ece0ad6d34969abbc7686e5d9be247
benny1693 <benedetto.cosentino@studenti.unipd.it>
2018-12-10 19:25:12 +0100

    Aggiornamento al tutorato del 10/12/2018

---
+++ todo.cpp
@@ -0,0 +1,18 @@
+#include "todo.h"
+
+std::ostream& operator<<(std::ostream& out,
+												 const ToDo& t)
+{
+	out << t.text;
+	return out;
+}
+
+ToDo::ToDo(string t) : text(t) {}
+
+bool ToDo::operator==(const ToDo &t) const {
+	return this->text == t.text;
+}
+
+bool ToDo::operator!=(const ToDo & t) const {
+	return !(*this == t);
+}
